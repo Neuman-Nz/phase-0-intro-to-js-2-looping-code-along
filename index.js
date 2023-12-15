13f6897feb0de25d8fe4841ab78d83f59a188962
neuman <owiroadams@gmail.com>
2023-12-16 01:06:09 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name){
+    let message = name.map(
+        (name) => `Thank you, ${name}, for the wonderful surprise gift!`
+    );
+    return message;
+
+}
+
+function countDown(num) {
+    num = 0;
+    for(let i=0; i<11; i++){
+        num++
+        console.log(i);
+    }
+};
